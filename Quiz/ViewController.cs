618a09881e2e5f92b079a8b95ed253b6a8461bf4
Daniel Ayala <dmayala001@gmail.com>
2016-08-08 14:48:03 -0700

    Add iOS Quiz App.

---
+++ Quiz/ViewController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace Quiz
+{
+    public partial class ViewController : UIViewController
+    {
+        private List<string> _questions = new List<string>
+        {
+            "From what is cognac made?",
+            "What is 7+7?",
+            "What is the capital of Vermont?"
+        };
+
+        private List<string> _answers = new List<string>
+        {
+            "Grapes",
+            "14",
+            "Montpelier"
+        };
+
+        private int _currentQuestionIndex = 0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            QuestionLabel.Text = _questions[_currentQuestionIndex];
+
+            NextQuestionButton.TouchUpInside += ShowNextQuestion;
+            ShowAnswerButton.TouchUpInside += ShowAnswer;
+        }
+
+        private void ShowNextQuestion(object sender, EventArgs e)
+        {
+            _currentQuestionIndex++;
+            if (_currentQuestionIndex == _questions.Count)
+            {
+                _currentQuestionIndex = 0;
+            }
+
+            var question = _questions[_currentQuestionIndex];
+            QuestionLabel.Text = question;
+            AnswerLabel.Text = "???";
+        }
+
+        private void ShowAnswer(object sender, EventArgs e)
+        {
+            var answer = _answers[_currentQuestionIndex];
+            AnswerLabel.Text = answer;
+        }
+    }
+}
---
+++ Quiz/ViewController.designer.cs
@@ -0,0 +1,56 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Quiz
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel AnswerLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton NextQuestionButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel QuestionLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ShowAnswerButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (AnswerLabel != null) {
+                AnswerLabel.Dispose ();
+                AnswerLabel = null;
+            }
+
+            if (NextQuestionButton != null) {
+                NextQuestionButton.Dispose ();
+                NextQuestionButton = null;
+            }
+
+            if (QuestionLabel != null) {
+                QuestionLabel.Dispose ();
+                QuestionLabel = null;
+            }
+
+            if (ShowAnswerButton != null) {
+                ShowAnswerButton.Dispose ();
+                ShowAnswerButton = null;
+            }
+        }
+    }
+}
